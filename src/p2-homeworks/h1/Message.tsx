fe320155ff93067590fc4b5ca21e61764b8bf2a7
Dmitry <incubator-it@protonmail.com>
2022-05-02 12:50:04 +0300

    ignatHomework_stage1

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,14 +1,27 @@
 import React from 'react'
+import s from './Message.module.css'
 
 
+type MessagePropsType = {
+    avatar: string
+    name: string
+    message: string
+    time: string
+}
 
-
-function Message() {
+export function Message(props: MessagePropsType) {
     return (
-        <div>
-
+        <div className={s.message}>
+            <div className={s.window}>
+                <div>{props.name}</div>
+                <div>{props.message}</div>
+                <span className={s.time}>{props.time}</span>
+            </div>
+            <div>
+                <img src="https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg"
+                     alt="ava"/>
+            </div>
         </div>
     )
 }
 
-export default Message
